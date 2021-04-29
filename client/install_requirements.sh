3566d5f0817b78a627001778c162b89980261cb9
Ivan Ignatyev <ignatev.is@phystech.edu>
2021-04-30 01:53:09 +0300

    Hello world client

---
+++ client/install_requirements.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install build-essential perl python git
+sudo apt-get install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev
+sudo apt-get install flex bison gperf libicu-dev libxslt-dev ruby
+pip3 install -r requirements.txt
