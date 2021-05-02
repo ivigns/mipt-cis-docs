faf1aeaceccb771bf448df32e80a1eeb55c84ea3
Ivan Ignatyev <ignatev.is@phystech.edu>
2021-05-02 18:07:35 +0300

    Add icon to client app

--- client/build_ubuntu.sh
+++ client/build_ubuntu.sh
@@ -1,1 +1,2 @@
+pyrcc5 client/resourses.qrc -o client/resourses.py
 pyinstaller -wF -n MiptCisDocs -i client/docs_icon.ico --add-data 'client/docs_icon.ico:.' client/main.py
--- client/build_windows.sh
+++ client/build_windows.sh
@@ -1,1 +1,2 @@
- pyinstaller.exe -wF -n MiptCisDocs -i client/docs_icon.ico --add-data 'client/docs_icon.ico;.' client/main.py
+pyrcc5 client/resourses.qrc -o client/resourses.py
+pyinstaller.exe -wF -n MiptCisDocs -i client/docs_icon.ico --add-data 'client/docs_icon.ico;.' client/main.py
